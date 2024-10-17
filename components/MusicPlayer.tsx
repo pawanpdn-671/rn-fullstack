7db91459c0f7bf899eb1c1e5b6725e710ae91705
pawanpdn-671 <pawanpradhanpc671@gmail.com>
2024-10-17 18:19:30 +0530

    update:setting up the player in the ui

--- components/MusicPlayer.tsx
+++ components/MusicPlayer.tsx
@@ -1,9 +1,41 @@
-import {Dimensions, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  ActivityIndicator,
+  Dimensions,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {addTrack, setupPlayer} from '../musicPlayerServices';
 
 const {height} = Dimensions.get('window');
 
 const MusicPlayer = () => {
+  const [isPlayerReady, setIsPlayerReady] = useState(false);
+
+  async function setup() {
+    let isSetup = await setupPlayer();
+
+    if (isSetup) {
+      await addTrack();
+    }
+
+    setIsPlayerReady(isSetup);
+  }
+
+  useEffect(() => {
+    setup();
+  }, []);
+
+  if (!isPlayerReady) {
+    return (
+      <SafeAreaView>
+        <ActivityIndicator />
+      </SafeAreaView>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>MusicPlayer</Text>
