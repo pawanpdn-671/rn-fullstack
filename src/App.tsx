57fe7cca23fd61ad39c355daa3730246ed2a64b8
pawanpdn-671 <pawanpradhanpc671@gmail.com>
2024-10-26 23:43:49 +0530

    update:setting up root navigator stack

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import {SafeAreaView, ScrollView, StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
-import ActionCard from '../components/ActionCard';
-import BgChanger from './screens/BgChanger';
+import React from 'react';
 import ContactList from '../components/ContactList';
-import CurrencyConverter from '../components/CurrencyConverter';
-import ElevatedCard from '../components/ElevatedCard';
-import FancyCard from '../components/FancyCard';
-import FlatCard from '../components/FlatCard';
-import PasswordGenerator from './screens/PasswordGenerator';
-import RollDice from './screens/RollDice';
-import TicTacToe from './screens/TicTacToe';
 import MusicPlayer from './screens/MusicPlayerScreen';
+import TicTacToe from './screens/TicTacToe';
+
+export type RootStackParamList = {
+  Home: undefined;
+  Explore: {exploreId: string};
+  Music: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <StatusBar />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={ContactList}
+          options={{
+            title: 'Contact List',
+          }}
+        />
+        <Stack.Screen
+          name="Explore"
+          component={TicTacToe}
+          options={{
+            title: 'Explore',
+          }}
+        />
+        <Stack.Screen
+          name="Music"
+          component={MusicPlayer}
+          options={{
+            title: 'Music Player',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
